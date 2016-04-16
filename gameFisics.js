eeb8ddb086774a2a8e621539d74f0f75ace2c4ce
FCO <fernandocorrea@gmail.com>
2016-04-16 00:25:33 -0300

    Create abstraction for lives. Finalize #11

--- gameFisics.js
+++ gameFisics.js
@@ -1,3 +1,34 @@
+function Lives(lives) {
+	if(lives != null)
+		this.lives = lives;
+}
+
+Lives.prototype = {
+	lives:			1,
+	solid:			false,
+	x:			0,
+	y:			0,
+	color:			"#000000",
+	title:			"Lives",
+	separator:		": ",
+	on_death:		function(){},
+	on_finish_lives:	function(){},
+
+	died:	function() {
+		this.lives -= 1;
+		this.on_death();
+		if(this.lives <= 0)
+			this.on_finish_lives();
+	},
+	newLives:	function(lives) {
+		this.lives += lives == undefined ? 1 : lives;
+	},
+	retangle_definition: function(){},
+	draw: function(screen) {
+		screen.ctx.fillText(this.title + this.separator + this.lives, 0, 0);
+	}
+};
+
 function ScoreMultiplicator(score) {
    this.score = score;
 }
