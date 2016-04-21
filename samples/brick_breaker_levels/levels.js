b03389d9a7e8cde03e335992e2332eb502631243
Fernando Oliveira <fco@cpan.org>
2016-04-21 18:52:57 -0300

    untrack levels.js file

--- samples/brick_breaker_levels/levels.js
+++
@@ -1,2 +0,0 @@
-exports.level1 = require('fs').readFileSync('/Users/fernando/JSGames/samples/brick_breaker_levels/level1.lvl');
-exports.level2 = require('fs').readFileSync('/Users/fernando/JSGames/samples/brick_breaker_levels/level2.lvl');
