724ec5099fca47f3d6cbeacdda0f79b95c8eb8ea
Pedro Lira <pedrohml@gmail.com>
2016-04-22 17:24:02 -0300

    Move brickbreaker tasks to right directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,3 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var insert = require('gulp-insert');
-var replace = require('gulp-replace');
 
 gulp.task('default', function() {});
-
-gulp.task('brickbreaker:levels', function() {
-	var levelsPath = 'samples/brickbreaker/levels/';
-	gulp.src(levelsPath + '*.lvl')
-	.pipe(replace("\n", "\\n"))
-	.pipe(insert.wrap('"', '"'))
-	.pipe(concat('levels.js', { newLine: ',' }))
-	.pipe(insert.wrap('module.exports = [', '];'))
-	.pipe(gulp.dest(levelsPath));
-});
---
+++ samples/brickbreaker/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var insert = require('gulp-insert');
+var replace = require('gulp-replace');
+
+gulp.task('default', function() {});
+
+gulp.task('brickbreaker:levels', function() {
+	var levelsPath = 'levels/';
+	gulp.src(levelsPath + '*.lvl')
+	.pipe(replace("\n", "\\n"))
+	.pipe(insert.wrap('"', '"'))
+	.pipe(concat('levels.js', { newLine: ',' }))
+	.pipe(insert.wrap('module.exports = [', '];'))
+	.pipe(gulp.dest(levelsPath));
+});
