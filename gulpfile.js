e9cc51067262655c96c621adfcff4c7e6cc3438b
FCO <fernandocorrea@gmail.com>
2016-04-24 23:43:44 -0300

    WiP: playing with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
-var gulp = require('gulp');
+var gulp	= require('gulp');
+var requireDir	= require("require-dir-all");
+
+requireDir("./samples", {recursive: true, includeFiles: /^gulpfile.js$/});
 
 gulp.task('default', function() {});
--- samples/brickbreaker/gulpfile.js
+++ samples/brickbreaker/gulpfile.js
@@ -1,7 +1,10 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var insert = require('gulp-insert');
-var replace = require('gulp-replace');
+var gulp	= require('gulp');
+var concat	= require('gulp-concat');
+var insert	= require('gulp-insert');
+var replace	= require('gulp-replace');
+var browserify	= require("gulp-browserify2");
+var inject	= require('gulp-inject');
+var webserver	= require('gulp-webserver');
 
 gulp.task('default', function() {});
 
@@ -13,4 +16,40 @@ gulp.task('brickbreaker:levels', function() {
 	.pipe(concat('levels.js', { newLine: ',' }))
 	.pipe(insert.wrap('module.exports = [', '];'))
 	.pipe(gulp.dest(levelsPath));
+});
+
+gulp.task('brickbreaker:build', ["brickbreaker:levels"], function() {
+	var js = gulp
+		.src("samples/brickbreaker/index.js")
+		.pipe(browserify({
+			fileName: "brickbreaker.js",
+			transform: [require("brfs"), require("require-globify")],
+			options: {
+				debug: true
+			}
+		}))
+		.pipe(gulp.dest("build"))
+	;
+
+	gulp
+		.src("samples/brickbreaker/*.html")
+		.pipe(gulp.dest("build"))
+		.pipe(inject(js, {relative: true}))
+		.pipe(gulp.dest("build"))
+	;
+});
+
+gulp.task("brickbreaker:watch", ["brickbreaker:build"], function() {
+	gulp.watch(["index.js", "screen.js", "elements/**.js", "samples/brickbreaker/**.js", "samples/brickbreaker/**.lvl", "**.json"], ['brickbreaker:build']);
+});
+
+gulp.task("brickbreaker", ["brickbreaker:watch"], function() {
+	gulp
+		.src("build/")
+		.pipe(webserver({
+			livereload: true,
+			directoryListing: false,
+			open: true
+		}))
+	;
 });
